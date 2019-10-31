94ecb9cf2271e50125358f6abd4327497835fa6b
1586951660 <1586951660@qq.com>
2019-11-01 00:40:34 +0800

    '完成了产品管理和栏目管理'

--- e洁家政服务平台/src/store/modules/product.js
+++ e洁家政服务平台/src/store/modules/product.js
@@ -1,73 +1,67 @@
 import request from '@/utils/request'
-
+import {get,post,post_array} from '@/utils/request'
 export default {
-  namespaced:true,
-  state:{
-    products:[],
-    visible:false,
-    title:"添加产品信息"
-  },
-  getters:{
-    productSize(state){
-      return state.products.length;
-    },
-    orderProduct:(state)=>{
-      return function(flag){
-        state.products.sort((a,b)=>{
-          if(a[flag] > b[flag]){
-            return -1;
-          } else {
-            return 1;
-          }
-        })
-        return state.products;
-      }
-    }
-  },
-  mutations:{
-    showModal(state){
-      state.visible = true;
-    },
-    closeModal(state){
-      state.visible = false;
-    },
-    refreshProducts(state,products){
-      state.products = products;
-    },
-    setTitle(state,title){
-      state.title = title;
-    }
-  },
-  actions:{
-    async batchDeleteProduct(context,ids){
-      // 1. 批量删除
-      let response = await request.post("/product/batchDelete",{ids})
-      // 2. 分发
-      context.dispatch("findAllProducts");
-      // 3. 返回结果
-      return response;
-    },
-    async deleteProductById(context,id){
-      let response = await request.get("/product/deleteById?id="+id);
-      context.dispatch("findAllProducts");
-      return response;
-    },
-    async findAllProducts(context){
-      // 1. ajax查询
-      let response = await request.get("/product/findAll");
-      // 2. 将查询结果更新到state中
-      context.commit("refreshProducts",response.data);
-    },
-    // payload 顾客信息
-    async saveOrUpdateProduct({commit,dispatch},payload){
-      // 1. 保存或更新
-      let response = await request.post("/product/saveOrUpdate",payload)
-      // 2. 刷新页面
-      dispatch("findAllProducts");
-      // 3. 关闭模态框
-      commit("closeModal");
-      // 4. 提示
-      return response;
-    }
-  }
+    namespaced:true,
+        state:{
+            products:[],
+            visible:false,
+            title:"添加产品信息",
+            product:{}  //当前产品的信息
+        },
+        getters:{
+
+        },
+        mutations:{
+            refreshProduct(state,products){
+                state.products = products;
+            },
+            refreshProducts(state,product){
+                state.product = product;
+            },
+            showModal(state){
+                state.visible = true;
+            },
+            closeModal(state){
+                state.visible = false;
+            },
+            setTitle(state,title){
+                state.title = title;
+            }
+        },
+        actions:{
+            //根据id查询产品信息
+            async findProductById(context,id){
+                let response = await get("/product/findById",{id})
+                context.commit('refreshProducts',response.data);
+            },
+            //查询数据
+            async findAllProduct(context){
+                //1.ajax查询
+                let response = await request.get("/product/findAll");
+                //2.将查询结果更新到state中
+                context.commit("refreshProduct",response.data);
+            },
+            async deleteProductById(context,id){
+                let response = await request.get("/product/deleteById?id="+id);
+                context.dispatch("findAllProduct");
+                return response;
+            },
+            async saveOrUpdateProduct({commit,dispatch},playload){
+                let response = await post("/product/saveOrUpdate",playload)
+                //刷新页面
+                dispatch("findAllProduct");
+                //关闭模态框
+                commit("closeModal");
+                //提示
+                return response; 
+            },
+            //批量删除
+            async batchDeleteProduct(context,ids){
+                let response = await post_array("/product/batchDelete",{ids});
+                //分发
+                context.dispatch("findAllProduct");
+                //返回结果
+                return response;
+            }    
+        }
 }
